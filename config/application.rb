6c0a8282ab14b683aaacc56b24911c7a4e96a9e5
r3drighthand <ryanmccool13@gmail.com>
2018-11-14 21:41:49 -0600

    Add dotenv to config

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'dotenv-rails'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module SortingHat
   class Application < Rails::Application
